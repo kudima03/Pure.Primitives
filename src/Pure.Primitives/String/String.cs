52e04d5b2253c3b5249a658a46343fbbc9b13a06
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-14 21:56:18 +0300

    Added String primitive implementation.

---
+++ src/Pure.Primitives/String/String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pure.Primitives.String;
+
+public sealed record String : IString
+{
+    private readonly string _value;
+
+    public String(string value)
+    {
+        _value = value;
+    }
+
+    string IString.Value => _value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
