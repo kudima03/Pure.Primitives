96e6f202cfc3bd94ff049551c68b18d738c92f69
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-14 21:56:33 +0300

    Added tests for String primitive.

---
+++ src/Tests/Pure.Primitives.Tests/String/StringTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.String;
+
+namespace Pure.Primitives.Tests.String;
+
+using String = Primitives.String.String;
+
+public sealed record StringTests
+{
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        const string testValue = "Test Value";
+        IString stringPrimitive = new String(testValue);
+        Assert.Equal(testValue, stringPrimitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new String("Test Value").GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new String("Test Value").ToString());
+    }
+}
