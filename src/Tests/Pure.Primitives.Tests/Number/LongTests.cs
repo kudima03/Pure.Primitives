0419f9afb4155b1a6527cda9d9fc1e09ae7329fa
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 09:37:44 +0300

    Added tests for long primitive.

---
+++ src/Tests/Pure.Primitives.Tests/Number/LongTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record LongTests
+{
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        const long testValue = 1910283761123132897;
+        ILong doublePrimitive = new Long(testValue);
+        Assert.Equal(testValue, doublePrimitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new Long(18459863293736745).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new Long(36597126725373475).ToString());
+    }
+}
