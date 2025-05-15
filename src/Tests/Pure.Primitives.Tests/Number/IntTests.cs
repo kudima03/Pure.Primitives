373897f7d04b33813cb6141ebb6f0e8917d6c809
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 09:36:28 +0300

    Added tests for int primitive.

---
+++ src/Tests/Pure.Primitives.Tests/Number/IntTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record IntTests
+{
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        const int testValue = 1910283761;
+        IInt doublePrimitive = new Int(testValue);
+        Assert.Equal(testValue, doublePrimitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new Int(1845986329).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new Int(365971267).ToString());
+    }
+}
