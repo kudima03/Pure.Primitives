8b4e04ca6294ffc73ef637017ad756bf57b03373
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 09:59:04 +0300

    Added day of week primitive implementation for every day of week.

---
+++ src/Pure.Primitives/DayOfWeek/Tuesday.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Number;
+using System;
+
+namespace Pure.Primitives.DayOfWeek;
+
+public sealed record Tuesday : IDayOfWeek
+{
+    IInt IDayOfWeek.DayNumber => new Int(2);
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ src/Pure.Primitives/DayOfWeek/Wednesday.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Number;
+using System;
+
+namespace Pure.Primitives.DayOfWeek;
+
+public sealed record Wednesday : IDayOfWeek
+{
+    IInt IDayOfWeek.DayNumber => new Int(3);
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
