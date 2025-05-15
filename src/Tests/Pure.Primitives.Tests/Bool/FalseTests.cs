32f92090df85b8a9c9e1ed33cbe30efd5f51c1a0
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 08:47:02 +0300

    Added tests for bool primitive implementation for false.

---
+++ src/Tests/Pure.Primitives.Tests/Bool/FalseTests.cs
@@ -0,0 +1,25 @@
+﻿using Pure.Primitives.Bool;
+
+namespace Pure.Primitives.Tests.Bool;
+
+public sealed record FalseTests
+{
+    [Fact]
+    public void InitializeFromDefaultConstructor()
+    {
+        IBool trueValue = new False();
+        Assert.False(trueValue.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new False().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new False().ToString());
+    }
+}
