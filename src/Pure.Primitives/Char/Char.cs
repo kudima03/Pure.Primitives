a932a78eeed0fe6db95e715fba88ccc2bd13aacd
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 09:04:07 +0300

    Added char primitive implementation.

---
+++ src/Pure.Primitives/Char/Char.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pure.Primitives.Char;
+
+public sealed record Char : IChar
+{
+    private readonly char _value;
+
+    public Char(char value)
+    {
+        _value = value;
+    }
+
+    char IChar.Value => _value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
