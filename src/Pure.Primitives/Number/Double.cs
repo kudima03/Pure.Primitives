e02b64fc42831c4b865e8c5dbae0f5c5b95c7282
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 09:27:50 +0300

    Added implementation for double primitive.

---
+++ src/Pure.Primitives/Number/Double.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record Double : IDouble
+{
+    private readonly double _value;
+
+    public Double(double value)
+    {
+        _value = value;
+    }
+
+    double IDouble.Value => _value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
