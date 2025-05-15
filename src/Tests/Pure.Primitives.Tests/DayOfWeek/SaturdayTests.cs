237ebb87276aa9e8a408608e95616991a22c2a3e
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 10:05:31 +0300

    Added tests for Saturday primitive.

---
+++ src/Tests/Pure.Primitives.Tests/DayOfWeek/SaturdayTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.DayOfWeek;
+
+namespace Pure.Primitives.Tests.DayOfWeek;
+
+public sealed record SaturdayTests
+{
+    [Fact]
+    public void InitializeFromDefaultConstructor()
+    {
+        const int expectedValue = 6;
+        IDayOfWeek dayOfWeek = new Saturday();
+        Assert.Equal(expectedValue, dayOfWeek.DayNumber.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new Saturday().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new Saturday().ToString());
+    }
+}
