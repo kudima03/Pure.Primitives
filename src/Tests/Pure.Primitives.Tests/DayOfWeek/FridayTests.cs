e45b839396ffbd4f163868e80c2c2f8389b712bf
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 10:03:45 +0300

    Added tests for Friday primitive.

---
+++ src/Tests/Pure.Primitives.Tests/DayOfWeek/FridayTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.DayOfWeek;
+
+namespace Pure.Primitives.Tests.DayOfWeek;
+
+public sealed record FridayTests
+{
+    [Fact]
+    public void InitializeFromDefaultConstructor()
+    {
+        const int expectedValue = 5;
+        IDayOfWeek dayOfWeek = new Friday();
+        Assert.Equal(expectedValue, dayOfWeek.DayNumber.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new Friday().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new Friday().ToString());
+    }
+}
