02adb04081c57ba651ab7a0e6ade66ff2fe26ac0
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 08:43:48 +0300

    Added bool primitive implementation for true.

---
+++ src/Pure.Primitives/Bool/True.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pure.Primitives.Bool;
+
+public sealed record True : IBool
+{
+    public True() { }
+
+    bool IBool.Value => true;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
