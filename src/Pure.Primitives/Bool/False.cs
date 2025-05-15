0e0302800976284edee3fd2a0047bf5b39d39de7
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-15 08:48:02 +0300

    Removed redundant constructors.

--- src/Pure.Primitives/Bool/False.cs
+++ src/Pure.Primitives/Bool/False.cs
@@ -4,8 +4,6 @@ namespace Pure.Primitives.Bool;
 
 public sealed record False : IBool
 {
-    public False() { }
-
     bool IBool.Value => false;
 
     public override int GetHashCode()
--- src/Pure.Primitives/Bool/True.cs
+++ src/Pure.Primitives/Bool/True.cs
@@ -4,8 +4,6 @@ namespace Pure.Primitives.Bool;
 
 public sealed record True : IBool
 {
-    public True() { }
-
     bool IBool.Value => true;
 
     public override int GetHashCode()
