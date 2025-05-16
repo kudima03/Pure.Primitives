95f9c14e05bae20b7ee701f088f05ba1b68fb23b
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-16 11:44:10 +0300

    Added implementation  for Guid primitive.

---
+++ src/Pure.Primitives/Guid/Guid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pure.Primitives.Guid;
+
+public sealed record Guid : IGuid
+{
+    private readonly System.Guid _value;
+
+    public Guid() : this(new System.Guid()) { }
+
+    public Guid(System.Guid value)
+    {
+        _value = value;
+    }
+
+    System.Guid IGuid.Value => _value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
