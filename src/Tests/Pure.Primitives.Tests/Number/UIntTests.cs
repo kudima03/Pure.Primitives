2ab55376a046b267d172f850379b813a5b60d6b5
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-16 12:26:09 +0300

    Added tests for UInt primitive.

---
+++ src/Tests/Pure.Primitives.Tests/Number/UIntTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record UIntTests
+{
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        const uint testValue = 3910283761;
+        IUInt uintPrimitive = new UInt(testValue);
+        Assert.Equal(testValue, uintPrimitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new UInt(754514523).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new UInt(918023479).ToString());
+    }
+}
