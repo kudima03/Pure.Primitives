de6fb1702b80f5685035c1ff4e842ee375e19196
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-16 11:50:42 +0300

    Added tests for Guid primitive.

---
+++ src/Tests/Pure.Primitives.Tests/Guid/GuidTests.cs
@@ -0,0 +1,35 @@
+﻿using Pure.Primitives.Guid;
+
+namespace Pure.Primitives.Tests.Guid;
+
+using Guid = Primitives.Guid.Guid;
+
+public sealed record GuidTests
+{
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        System.Guid testValue = new System.Guid();
+        IGuid guidPrimitive = new Guid(testValue);
+        Assert.Equal(testValue, guidPrimitive.Value);
+    }
+
+    [Fact]
+    public void InitializeFromEmptyConstructor()
+    {
+        IGuid guidPrimitive = new Guid();
+        Assert.True(System.Guid.TryParse(guidPrimitive.Value.ToString(), out System.Guid _));
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new Guid().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new Guid().ToString());
+    }
+}
