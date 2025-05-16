af2a1709c4d5ba443a2233e89c28b4d0d38f53e0
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-16 12:20:35 +0300

    Added implementation for uint type.

---
+++ src/Pure.Primitives/Number/UInt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record UInt : IUInt
+{
+    private readonly uint _value;
+
+    public UInt(uint value)
+    {
+        _value = value;
+    }
+
+    uint IUInt.Value => _value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
