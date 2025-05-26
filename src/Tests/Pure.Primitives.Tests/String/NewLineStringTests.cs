2e1ae29468c1dbe402a269bfe0728786849f7a0d
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-26 09:30:06 +0300

    Implemented tests for NewLineString.

---
+++ src/Tests/Pure.Primitives.Tests/String/NewLineStringTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.String;
+
+namespace Pure.Primitives.Tests.String;
+
+public sealed record NewLineStringTests
+{
+    [Fact]
+    public void ReturnCorrectValue()
+    {
+        IString stringPrimitive = new NewLineString();
+        Assert.Equal(Environment.NewLine, stringPrimitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new NewLineString().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new NewLineString().ToString());
+    }
+}
