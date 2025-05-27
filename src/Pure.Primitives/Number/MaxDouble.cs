498564a58bfab83dd95f3ddb8cfad0d2f27675c8
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 13:46:57 +0300

    Implemented MaxDouble.

---
+++ src/Pure.Primitives/Number/MaxDouble.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record MaxDouble : INumber<double>
+{
+    double INumber<double>.Value => double.MaxValue;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
