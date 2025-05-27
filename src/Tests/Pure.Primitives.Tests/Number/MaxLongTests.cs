54df8b3b538fb5eaafbdd9fb0d084d6f0d5b777b
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 13:51:30 +0300

    Implemented MaxLongTests.

---
+++ src/Tests/Pure.Primitives.Tests/Number/MaxLongTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MaxLongTests
+{
+    [Fact]
+    public void InitializeAsMaxLong()
+    {
+        INumber<long> primitive = new MaxLong();
+        Assert.Equal(long.MaxValue, primitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxLong().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxLong().ToString());
+    }
+}
