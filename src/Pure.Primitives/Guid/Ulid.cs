a64c7c9a1457050ace57da667fffc344676a6dde
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 16:02:36 +0300

    Created Ulid primitve.

---
+++ src/Pure.Primitives/Guid/Ulid.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Abstractions.Guid;
+using System;
+
+namespace Pure.Primitives.Guid;
+
+public sealed record Ulid : IGuid
+{
+    private readonly System.Guid _value;
+
+    public Ulid() : this(System.Guid.CreateVersion7()) { }
+
+    public Ulid(System.Guid value)
+    {
+        _value = value;
+    }
+
+    System.Guid IGuid.Value => _value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
