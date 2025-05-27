6d46779a95f8df1251639de9c95acbd92561745d
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 13:51:09 +0300

    Implemented MaxLong.

---
+++ src/Pure.Primitives/Number/MaxLong.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record MaxLong : INumber<long>
+{
+    long INumber<long>.Value => long.MaxValue;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
