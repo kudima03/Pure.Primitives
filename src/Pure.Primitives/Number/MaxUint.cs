ba1d4be5578eb6e2adb49f32fdf78615013d4b05
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 14:50:57 +0300

    Implemented MaxUint

---
+++ src/Pure.Primitives/Number/MaxUint.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record MaxUint : INumber<uint>
+{
+    uint INumber<uint>.Value => uint.MaxValue;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
