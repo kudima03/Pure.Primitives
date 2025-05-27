81fa78e064b6007e4d7eb775020bf3bdf9946358
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 13:46:49 +0300

    Implemented MinDouble.

---
+++ src/Pure.Primitives/Number/MinDouble.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record MinDouble : INumber<double>
+{
+    double INumber<double>.Value => double.MinValue;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
