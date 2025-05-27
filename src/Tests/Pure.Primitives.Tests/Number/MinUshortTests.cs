dfb00ff4c7dcb551a74c6ece092793f0aa99ee81
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 14:46:15 +0300

    implemented MinUshortTests.

---
+++ src/Tests/Pure.Primitives.Tests/Number/MinUshortTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MinUshortTests
+{
+    [Fact]
+    public void InitializeAsMinInt()
+    {
+        INumber<ushort> primitive = new MinUshort();
+        Assert.Equal(ushort.MinValue, primitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinUshort().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinUshort().ToString());
+    }
+}
