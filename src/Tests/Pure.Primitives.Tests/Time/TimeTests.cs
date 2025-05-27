015c8754fe4e483175adcda6b3adb1b370b69ed3
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 20:32:05 +0300

    Implemented tests for Time primitive.

---
+++ src/Tests/Pure.Primitives.Tests/Time/TimeTests.cs
@@ -0,0 +1,53 @@
+﻿using Pure.Primitives.Abstractions.Time;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Time;
+
+using Time = Primitives.Time.Time;
+
+public sealed record TimeTests
+{
+    [Fact]
+    public void InitializeFromTimeOnly()
+    {
+        TimeOnly timeOnly = new TimeOnly(15, 30, 30, 30, 30);
+
+        ITime time = new Time(timeOnly);
+
+        Assert.Equal(timeOnly, time.TimeValue);
+    }
+
+    [Fact]
+    public void InitializeFromFullNumbers()
+    {
+        TimeOnly timeOnly = new TimeOnly(15, 30, 30, 30, 30);
+
+        ITime time = new Time(timeOnly);
+
+        Assert.Equal(timeOnly, time.TimeValue);
+    }
+
+    [Fact]
+    public void InitializeFromNumbers()
+    {
+        TimeOnly timeOnly = new TimeOnly(15, 30, 30);
+
+        ITime time = new Time(timeOnly);
+
+        Assert.Equal(timeOnly, time.TimeValue);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() =>
+            new Time(new UShort(1), new UShort(1), new UShort(2000)).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() =>
+            new Time(new UShort(1), new UShort(1), new UShort(2000)).ToString());
+    }
+}
