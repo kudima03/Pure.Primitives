4c7ef1258cd631d839de567d085f360cf95285f8
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 13:47:14 +0300

    Implemented MinDoubleTests.

---
+++ src/Tests/Pure.Primitives.Tests/Number/MinDoubleTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MinDoubleTests
+{
+    [Fact]
+    public void InitializeAsMinDouble()
+    {
+        INumber<double> primitive = new MinDouble();
+        Assert.Equal(double.MinValue, primitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinDouble().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinDouble().ToString());
+    }
+}
