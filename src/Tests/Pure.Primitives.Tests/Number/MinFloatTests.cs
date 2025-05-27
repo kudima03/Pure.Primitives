e3e99eeea2864f3fafdd6e8dc13ee9748995d0a9
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 14:34:41 +0300

    Implemented MinFloatTests.

---
+++ src/Tests/Pure.Primitives.Tests/Number/MinFloatTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MinFloatTests
+{
+    [Fact]
+    public void InitializeAsMinFloat()
+    {
+        INumber<float> primitive = new MinFloat();
+        Assert.Equal(float.MinValue, primitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinFloat().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinFloat().ToString());
+    }
+}
