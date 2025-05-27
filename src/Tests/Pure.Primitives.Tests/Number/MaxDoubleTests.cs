deb1311b3596acc9f44c618aa9ce7965c6f25efe
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-27 13:47:19 +0300

    Implemented MaxDoubleTests.

---
+++ src/Tests/Pure.Primitives.Tests/Number/MaxDoubleTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MaxDoubleTests
+{
+    [Fact]
+    public void InitializeAsMaxDouble()
+    {
+        INumber<double> primitive = new MaxDouble();
+        Assert.Equal(double.MaxValue, primitive.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxDouble().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxDouble().ToString());
+    }
+}
