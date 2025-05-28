28167622067ed1537ba6bf571d801c380ec672f9
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-28 14:12:47 +0300

    Implemented tests for CurrentDate

---
+++ src/Tests/Pure.Primitives.Tests/Date/CurrentDateTests.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Abstractions.Date;
+using Pure.Primitives.Date;
+
+namespace Pure.Primitives.Tests.Date;
+
+public sealed record CurrentDateTests
+{
+    [Fact]
+    public void InitializeCorrectly()
+    {
+        IDate date = new CurrentDate();
+
+        Assert.Equal(DateOnly.FromDateTime(System.DateTime.Today),
+            new DateOnly(date.Year.NumberValue, date.Month.NumberValue, date.Day.NumberValue));
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new CurrentDate().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new CurrentDate().ToString());
+    }
+}
