207ece77710c4e7a45554cccaa297273dbc73d9f
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-28 14:31:56 +0300

    Implemented tests for CurrentTime

---
+++ src/Tests/Pure.Primitives.Tests/Time/CurrentTimeTests.cs
@@ -0,0 +1,34 @@
+﻿using Pure.Primitives.Abstractions.Time;
+using Pure.Primitives.Time;
+
+namespace Pure.Primitives.Tests.Time;
+
+public sealed record CurrentTimeTests
+{
+    [Fact]
+    public void InitializeCorrectly()
+    {
+        TimeOnly time = new TimeOnly(System.DateTime.Now.Hour,
+            System.DateTime.Now.Minute, 
+            System.DateTime.Now.Second);
+
+        ITime currentTime = new CurrentTime();
+
+        Assert.Equal(time,
+            new TimeOnly(currentTime.Hour.NumberValue,
+                currentTime.Minute.NumberValue,
+                currentTime.Second.NumberValue));
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new CurrentTime().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new CurrentTime().ToString());
+    }
+}
