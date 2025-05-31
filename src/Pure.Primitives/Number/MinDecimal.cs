787c3edd4cb78a6918f864c1b0817a27cfb8afbb
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-31 22:02:05 +0300

    Implemented MinDecimal.

---
+++ src/Pure.Primitives/Number/MinDecimal.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record MinDecimal : INumber<decimal>
+{
+    decimal INumber<decimal>.NumberValue => decimal.MinValue;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
