507d6cb1145a7348dd6dc67736cc1b129adf8262
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-31 22:00:59 +0300

    Implemented Decimal primitive.

---
+++ src/Pure.Primitives/Number/Decimal.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using System;
+
+namespace Pure.Primitives.Number;
+
+public sealed record Decimal : INumber<decimal>
+{
+    private readonly decimal _value;
+
+    public Decimal(decimal value)
+    {
+        _value = value;
+    }
+
+    decimal INumber<decimal>.NumberValue => _value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
