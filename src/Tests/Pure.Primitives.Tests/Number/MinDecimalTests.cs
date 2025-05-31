a0510f3eae2ae9d8892a16b24f033d87384ffebe
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-31 22:06:17 +0300

    Implemented tests for MinDecimal.

---
+++ src/Tests/Pure.Primitives.Tests/Number/MinDecimalTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MinDecimalTests
+{
+    [Fact]
+    public void InitializeAsMinDecimal()
+    {
+        INumber<decimal> primitive = new MinDecimal();
+        Assert.Equal(decimal.MinValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxDouble().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxDouble().ToString());
+    }
+}
