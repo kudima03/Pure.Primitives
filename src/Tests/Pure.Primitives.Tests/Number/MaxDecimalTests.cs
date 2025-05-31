665c5cbe8ce14a5eb832b5e67c0bbc16d3e35d9d
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-31 22:06:09 +0300

    Implemented tests for MaxDecimal.

---
+++ src/Tests/Pure.Primitives.Tests/Number/MaxDecimalTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MaxDecimalTests
+{
+    [Fact]
+    public void InitializeAsMaxDecimal()
+    {
+        INumber<decimal> primitive = new MaxDecimal();
+        Assert.Equal(decimal.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxDouble().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxDouble().ToString());
+    }
+}
