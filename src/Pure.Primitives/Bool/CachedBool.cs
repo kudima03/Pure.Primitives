fdd0ec2d735c08e814b01eab90a28bf2535742cd
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-10 09:41:47 +0300

    Implemented CachedBool.

---
+++ src/Pure.Primitives/Bool/CachedBool.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using System;
+
+namespace Pure.Primitives.Bool;
+
+public sealed record CachedBool : IBool
+{
+    private readonly Lazy<bool> _lazyValue;
+
+    public CachedBool(IBool value) : this(new Lazy<bool>(() => value.BoolValue)) { }
+
+    private CachedBool(Lazy<bool> lazyValue)
+    {
+        _lazyValue = lazyValue;
+    }
+
+    bool IBool.BoolValue => _lazyValue.Value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
