d8f1d3c37778ea0e1f1e5cf2d82ec3e1f6de95e1
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-19 14:36:23 +0300

    Implemented tests for EmptyGuid.

---
+++ src/Tests/Pure.Primitives.Tests/Guid/EmptyGuidTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Guid;
+using Pure.Primitives.Guid;
+
+namespace Pure.Primitives.Tests.Guid;
+
+public sealed record EmptyGuidTests
+{
+    [Fact]
+    public void InitializeFromEmptyConstructorWithEmptyGuid()
+    {
+        IGuid guidPrimitive = new EmptyGuid();
+        Assert.Equal(System.Guid.Empty, guidPrimitive.GuidValue);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new EmptyGuid().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new EmptyGuid().ToString());
+    }
+}
