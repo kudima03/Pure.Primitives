39b7635becbb48637551563cf24dd3f181b7f78c
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-19 14:35:04 +0300

    Implemented EmptyGuid.

---
+++ src/Pure.Primitives/Guid/EmptyGuid.cs
@@ -0,0 +1,19 @@
+﻿using Pure.Primitives.Abstractions.Guid;
+using System;
+
+namespace Pure.Primitives.Guid;
+
+public sealed record EmptyGuid : IGuid
+{
+    public System.Guid GuidValue => System.Guid.Empty;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
