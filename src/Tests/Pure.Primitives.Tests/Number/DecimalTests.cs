e3d8e5ad3b16ec00026026d69f3ea385105a4eee
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-24 22:41:37 +0300

    Added tests for Decimal

--- src/Tests/Pure.Primitives.Tests/Number/DecimalTests.cs
+++ src/Tests/Pure.Primitives.Tests/Number/DecimalTests.cs
@@ -1,4 +1,5 @@
 ﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
 using Decimal = Pure.Primitives.Number.Decimal;
 
 namespace Pure.Primitives.Tests.Number;
@@ -6,7 +7,42 @@ namespace Pure.Primitives.Tests.Number;
 public sealed record DecimalTests
 {
     [Fact]
-    public void InitializeFromScalarConstructor()
+    public void InitializeFromShortConstructor()
+    {
+        INumber<decimal> doublePrimitive = new Decimal(new Int(short.MaxValue));
+        Assert.Equal(short.MaxValue, doublePrimitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromUShortConstructor()
+    {
+        INumber<decimal> doublePrimitive = new Decimal(new Int(ushort.MaxValue));
+        Assert.Equal(ushort.MaxValue, doublePrimitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromIntConstructor()
+    {
+        INumber<decimal> doublePrimitive = new Decimal(new Int(int.MaxValue));
+        Assert.Equal(int.MaxValue, doublePrimitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromUIntConstructor()
+    {
+        INumber<decimal> doublePrimitive = new Decimal(new UInt(uint.MaxValue));
+        Assert.Equal(uint.MaxValue, doublePrimitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromLongConstructor()
+    {
+        INumber<decimal> doublePrimitive = new Decimal(new Long(long.MaxValue));
+        Assert.Equal(long.MaxValue, doublePrimitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromDecimalConstructor()
     {
         INumber<decimal> doublePrimitive = new Decimal(0.1M + 0.2M);
         Assert.Equal(0.3M, doublePrimitive.NumberValue);
