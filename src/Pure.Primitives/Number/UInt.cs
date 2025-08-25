c6ad623e7653f15d2bcc872e8a0030fe7bbfce2e
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-25 12:15:45 +0300

    Added conversion ctors to UInt

--- src/Pure.Primitives/Number/UInt.cs
+++ src/Pure.Primitives/Number/UInt.cs
@@ -5,14 +5,20 @@ namespace Pure.Primitives.Number;
 
 public sealed record UInt : INumber<uint>
 {
-    private readonly uint _value;
+    private readonly Lazy<uint> _lazyValue;
 
     public UInt(uint value)
+        : this(new Lazy<uint>(() => value)) { }
+
+    public UInt(INumber<ushort> value)
+        : this(new Lazy<uint>(() => Convert.ToUInt32(value.NumberValue))) { }
+
+    private UInt(Lazy<uint> lazyValue)
     {
-        _value = value;
+        _lazyValue = lazyValue;
     }
 
-    uint INumber<uint>.NumberValue => _value;
+    uint INumber<uint>.NumberValue => _lazyValue.Value;
 
     public override int GetHashCode()
     {
