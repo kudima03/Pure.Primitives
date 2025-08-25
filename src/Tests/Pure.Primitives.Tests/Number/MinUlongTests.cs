2679678138b85d597394db572df21ddab3e09193
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-25 12:37:20 +0300

    Added MinUlongTests

---
+++ src/Tests/Pure.Primitives.Tests/Number/MinUlongTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MinUlongTests
+{
+    [Fact]
+    public void InitializeAsMaxUlong()
+    {
+        INumber<ulong> primitive = new MinUlong();
+        Assert.Equal(ulong.MinValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinUlong().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MinUlong().ToString());
+    }
+}
