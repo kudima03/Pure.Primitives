520b10b11eb891597fa9ac1ad98f06c970db26f0
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-25 12:30:46 +0300

    Added ULongTests

---
+++ src/Tests/Pure.Primitives.Tests/Number/ULongTests.cs
@@ -0,0 +1,41 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record ULongTests
+{
+    [Fact]
+    public void InitializeFromUShortConstructor()
+    {
+        INumber<ulong> primitive = new ULong(new MaxUshort());
+        Assert.Equal(ushort.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromUIntConstructor()
+    {
+        INumber<ulong> primitive = new ULong(new MaxUint());
+        Assert.Equal(uint.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        const ulong testValue = 1910283761123132897;
+        INumber<ulong> doublePrimitive = new ULong(testValue);
+        Assert.Equal(testValue, doublePrimitive.NumberValue);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new ULong(ulong.MaxValue).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new ULong(ulong.MaxValue).ToString());
+    }
+}
