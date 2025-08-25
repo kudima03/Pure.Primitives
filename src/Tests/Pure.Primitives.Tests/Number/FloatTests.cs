17bd545921f5100709420354e0f16ff85ac422e6
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-25 12:00:56 +0300

    Added tests for Float

--- src/Tests/Pure.Primitives.Tests/Number/FloatTests.cs
+++ src/Tests/Pure.Primitives.Tests/Number/FloatTests.cs
@@ -6,6 +6,41 @@ namespace Pure.Primitives.Tests.Number;
 public sealed record FloatTests
 {
     [Fact]
+    public void InitializeFromShortConstructor()
+    {
+        INumber<float> primitive = new Float(new MaxUshort());
+        Assert.Equal(ushort.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromUShortConstructor()
+    {
+        INumber<float> primitive = new Float(new Int(ushort.MaxValue));
+        Assert.Equal(ushort.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromIntConstructor()
+    {
+        INumber<float> primitive = new Float(new MaxInt());
+        Assert.Equal(int.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromUIntConstructor()
+    {
+        INumber<float> primitive = new Float(new MaxUint());
+        Assert.Equal(uint.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void InitializeFromLongConstructor()
+    {
+        INumber<float> primitive = new Float(new MaxLong());
+        Assert.Equal(long.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
     public void InitializeFromScalarConstructor()
     {
         const float testValue = 1.12837491280374129387837529183759012F;
