238d94819ec4ab13d8310cb251e56468fc76134c
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-25 12:44:35 +0300

    Added MaxShortTests

---
+++ src/Tests/Pure.Primitives.Tests/Number/MaxShortTests.cs
@@ -0,0 +1,26 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Tests.Number;
+
+public sealed record MaxShortTests
+{
+    [Fact]
+    public void InitializeAsMaxShort()
+    {
+        INumber<short> primitive = new MaxShort();
+        Assert.Equal(short.MaxValue, primitive.NumberValue);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxShort().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new MaxShort().ToString());
+    }
+}
