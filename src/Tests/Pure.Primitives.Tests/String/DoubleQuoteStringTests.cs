d09650e907a2cc243b789d77b49e7eaf247ec974
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-05 11:08:04 +0300

    DoubleQuoteStringTests implemented.

---
+++ src/Tests/Pure.Primitives.Tests/String/DoubleQuoteStringTests.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using Pure.Primitives.Abstractions.Char;
+using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.String;
+
+namespace Pure.Primitives.Tests.String;
+
+public sealed record DoubleQuoteStringTests
+{
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        IString stringPrimitive = new DoubleQuoteString();
+        Assert.Equal("\"", stringPrimitive.TextValue);
+    }
+
+    [Fact]
+    public void EnumeratesAsTyped()
+    {
+        IEnumerable<IChar> stringPrimitive = new DoubleQuoteString();
+        Assert.True("\"".SequenceEqual(stringPrimitive.Select(x => x.CharValue)));
+    }
+
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        IEnumerable stringPrimitive = new DoubleQuoteString();
+
+        ICollection<IChar> symbols = [];
+
+        foreach (object symbol in stringPrimitive)
+        {
+            symbols.Add((symbol as IChar)!);
+        }
+
+        Assert.True("\"".SequenceEqual(symbols.Select(x => x.CharValue)));
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        _ = Assert.Throws<NotSupportedException>(() =>
+            new DoubleQuoteString().GetHashCode()
+        );
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        _ = Assert.Throws<NotSupportedException>(() =>
+            new DoubleQuoteString().ToString()
+        );
+    }
+}
