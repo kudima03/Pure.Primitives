64acbdce73eca4ed2d55002bbe3105b820ef5f83
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-05 11:40:36 +0300

    Implemented RightSquareBracketStringTests

---
+++ src/Tests/Pure.Primitives.Tests/String/RightSquareBracketStringTests.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using Pure.Primitives.Abstractions.Char;
+using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.String;
+
+namespace Pure.Primitives.Tests.String;
+
+public sealed record RightSquareBracketStringTests
+{
+    [Fact]
+    public void InitializeFromScalarConstructor()
+    {
+        IString stringPrimitive = new RightSquareBracketString();
+        Assert.Equal("]", stringPrimitive.TextValue);
+    }
+
+    [Fact]
+    public void EnumeratesAsTyped()
+    {
+        IEnumerable<IChar> stringPrimitive = new RightSquareBracketString();
+        Assert.True("]".SequenceEqual(stringPrimitive.Select(x => x.CharValue)));
+    }
+
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        IEnumerable stringPrimitive = new RightSquareBracketString();
+
+        ICollection<IChar> symbols = [];
+
+        foreach (object symbol in stringPrimitive)
+        {
+            symbols.Add((symbol as IChar)!);
+        }
+
+        Assert.True("]".SequenceEqual(symbols.Select(x => x.CharValue)));
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        _ = Assert.Throws<NotSupportedException>(() =>
+            new RightSquareBracketString().GetHashCode()
+        );
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        _ = Assert.Throws<NotSupportedException>(() =>
+            new RightSquareBracketString().ToString()
+        );
+    }
+}
