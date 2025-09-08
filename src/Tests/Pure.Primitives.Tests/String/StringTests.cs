50129752e4b83047b98e59cc09d49bca9a9420af
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-08 16:53:38 +0300

    StringTests improved.

--- src/Tests/Pure.Primitives.Tests/String/StringTests.cs
+++ src/Tests/Pure.Primitives.Tests/String/StringTests.cs
@@ -1,6 +1,22 @@
 using System.Collections;
+using System.Globalization;
+using Pure.Primitives.Abstractions.Bool;
 using Pure.Primitives.Abstractions.Char;
+using Pure.Primitives.Abstractions.Date;
+using Pure.Primitives.Abstractions.DateTime;
+using Pure.Primitives.Abstractions.DayOfWeek;
+using Pure.Primitives.Abstractions.Guid;
+using Pure.Primitives.Abstractions.Number;
 using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.Abstractions.Time;
+using Pure.Primitives.Bool;
+using Pure.Primitives.DayOfWeek;
+using Pure.Primitives.Number;
+using Pure.Primitives.Random.Char;
+using Pure.Primitives.Random.Date;
+using Pure.Primitives.Random.DateTime;
+using Pure.Primitives.Random.Number;
+using Pure.Primitives.Random.Time;
 
 namespace Pure.Primitives.Tests.String;
 
@@ -8,6 +24,170 @@ using String = Primitives.String.String;
 
 public sealed record StringTests
 {
+    [Fact]
+    public void InitializeFromBool()
+    {
+        IBool value = new False();
+        IString parsed = new String(value);
+        Assert.Equal(value.BoolValue.ToString(), parsed.TextValue);
+    }
+
+    [Fact]
+    public void InitializeFromChar()
+    {
+        IChar value = new RandomChar();
+        IString parsed = new String(value);
+        Assert.Equal(value.CharValue.ToString(), parsed.TextValue);
+    }
+
+    [Fact]
+    public void InitializeFromDate()
+    {
+        IDate value = new RandomDate();
+        IString parsed = new String(value);
+        Assert.Equal(
+            $"{value.Month.NumberValue}/{value.Day.NumberValue}/{value.Year.NumberValue}",
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromDateTime()
+    {
+        IDateTime value = new RandomDateTime();
+        IString parsed = new String(value);
+        Assert.Equal(
+            $"{value.Month.NumberValue}/{value.Day.NumberValue}/{value.Year.NumberValue} {value.Hour.NumberValue}:{value.Minute.NumberValue}:{value.Second.NumberValue}.{value.Millisecond.NumberValue}.{value.Nanosecond.NumberValue}",
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromTime()
+    {
+        ITime value = new RandomTime();
+        IString parsed = new String(value);
+        Assert.Equal(
+            $"{value.Hour.NumberValue}:{value.Minute.NumberValue}:{value.Second.NumberValue}.{value.Millisecond.NumberValue}.{value.Nanosecond.NumberValue}",
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromDayOfWeek()
+    {
+        IDayOfWeek value = new Monday();
+        IString parsed = new String(value);
+        Assert.Equal("Monday", parsed.TextValue);
+    }
+
+    [Fact]
+    public void InitializeFromGuid()
+    {
+        IGuid value = new Primitives.Guid.Guid();
+        IString parsed = new String(value);
+        Assert.Equal(value.GuidValue.ToString(), parsed.TextValue);
+    }
+
+    [Fact]
+    public void InitializeFromDecimal()
+    {
+        INumber<decimal> value = new RandomDecimal();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromDouble()
+    {
+        INumber<double> value = new RandomDouble();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromFloat()
+    {
+        INumber<float> value = new RandomFloat();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromInt()
+    {
+        INumber<int> value = new RandomInt();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromLong()
+    {
+        INumber<long> value = new Long(10);
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromShort()
+    {
+        INumber<short> value = new MaxShort();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromUint()
+    {
+        INumber<uint> value = new RandomUInt();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromUlong()
+    {
+        INumber<ulong> value = new MaxUlong();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
+    [Fact]
+    public void InitializeFromUshort()
+    {
+        INumber<ushort> value = new RandomUShort();
+        IString parsed = new String(value);
+        Assert.Equal(
+            value.NumberValue.ToString(CultureInfo.InvariantCulture),
+            parsed.TextValue
+        );
+    }
+
     [Fact]
     public void InitializeFromScalarConstructor()
     {
